4de209fbbfb5fe15368fa1b781ae33d26913b3da
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-01-25 22:11:47 +0200

    [ART-4] Added migration for Account table creating

---
+++ src/ArtAuction.Infrastructure.Data/Migrations/Migration_202201251800_CreatingAccountTable.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace ArtAuction.Infrastructure.Persistence.Migrations
+{
+    [Migration(202201251800)]
+    public class Migration_202201251800_CreatingAccountTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("account")
+                .WithColumn("account_id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(SystemMethods.NewGuid)
+                .WithColumn("user_id").AsGuid().NotNullable().Unique()
+                .WithColumn("sum").AsDecimal().NotNullable().WithDefaultValue(decimal.Zero)
+                .WithColumn("last_update").AsDateTime().NotNullable().WithDefaultValue(SystemMethods.CurrentDateTime);
+
+            Create.ForeignKey()
+                .FromTable("account").ForeignColumn("user_id")
+                .ToTable("user").PrimaryColumn("user_id");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("account");
+        }
+    }
+}
