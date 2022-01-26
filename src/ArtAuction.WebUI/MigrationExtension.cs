7988b9d9a71b965bc37e77aed0010a63c2785aac
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-01-26 20:14:19 +0200

    [ART-4] Fixed MigrationExtension

--- src/ArtAuction.WebUI/MigrationExtension.cs
+++ src/ArtAuction.WebUI/MigrationExtension.cs
@@ -11,7 +11,7 @@ namespace ArtAuction.WebUI
             using var scope = builder.ApplicationServices.CreateScope();
             
             var runner = scope.ServiceProvider.GetService<IMigrationRunner>();
-            runner?.MigrateDown(202201251800); runner?.MigrateUp();
+            runner?.MigrateUp();
 
             // uncomment it for migration revert
             // runner?.MigrateDown(202201251200);
