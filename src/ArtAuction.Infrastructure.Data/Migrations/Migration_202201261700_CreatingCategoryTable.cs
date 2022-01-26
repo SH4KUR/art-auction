1b05cc94eed59b23a8ce0c0b53f1a547bc331cf1
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-01-26 20:18:51 +0200

    [ART-4] Added migration for Category table creating

---
+++ src/ArtAuction.Infrastructure.Data/Migrations/Migration_202201261700_CreatingCategoryTable.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace ArtAuction.Infrastructure.Persistence.Migrations
+{
+    [Migration(202201261700)]
+    public class Migration_202201261700_CreatingCategoryTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("category")
+                .WithColumn("category_id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(SystemMethods.NewGuid)
+                .WithColumn("name").AsString(100).NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("category");
+        }
+    }
+}
