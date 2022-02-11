952809406fbbc2657852dce63b6ed759b772498e
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-02-11 17:22:03 +0200

    [ART-7] Added IUserRepository interface. Updated DI in Startup.cs.

---
+++ src/ArtAuction.Infrastructure.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using ArtAuction.Core.Application.Interfaces.Repositories;
+using ArtAuction.Core.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace ArtAuction.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public User GetUser(string login)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddUser(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateUser(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
