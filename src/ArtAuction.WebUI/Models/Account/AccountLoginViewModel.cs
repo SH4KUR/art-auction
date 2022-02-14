c618ce9ae2bc87ff8d1bbb69e4173035a03c9224
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-02-14 22:04:37 +0200

    [ART-11] Created simple Login and Registration pages

--- src/ArtAuction.WebUI/Models/Account/AccountLoginViewModel.cs
+++ src/ArtAuction.WebUI/Models/Account/AccountLoginViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace ArtAuction.WebUI.Models.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArtAuction.WebUI.Models.Account
 {
     public class AccountLoginViewModel
     {
+        [Required]
+        public string Login { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
     }
 }
