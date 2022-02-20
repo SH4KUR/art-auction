81baf95fb501c9362b764257257f7a3d3ce26424
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-02-20 19:15:19 +0200

    [ART-40] Added main AuctionCatalogController

---
+++ src/ArtAuction.WebUI/Controllers/AuctionCatalogController.cs
@@ -0,0 +1,20 @@
+﻿using ArtAuction.Core.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtAuction.WebUI.Controllers
+{
+    public class AuctionCatalogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = nameof(UserRole.Seller))]
+        public IActionResult AddAuction()
+        {
+            return View();
+        }
+    }
+}
