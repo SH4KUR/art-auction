4231bb9d51d732c6edd7806548eaf9f2a9cff2c5
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-03-05 19:08:49 +0200

    [ART-25] Added empty Lot page

---
+++ src/ArtAuction.WebUI/Controllers/LotController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtAuction.WebUI.Controllers
+{
+    public class LotController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        
+        public LotController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int auctionNumber)
+        {
+            return View();
+        }
+    }
+}
