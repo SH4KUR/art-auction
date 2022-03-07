87414d231c48097bcbb367b33752f67832d57968
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-03-07 19:22:37 +0200

    [ART-54] Base Profile page

---
+++ src/ArtAuction.WebUI/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtAuction.WebUI.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        
+        public ProfileController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
