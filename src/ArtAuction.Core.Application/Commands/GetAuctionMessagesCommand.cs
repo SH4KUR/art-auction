e502eaf2afafec90709177a8dd80e2b4c216d67a
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-06-16 16:35:43 +0300

    [ART-26] Added GetAuctionMessagesCommandHandler

---
+++ src/ArtAuction.Core.Application/Commands/GetAuctionMessagesCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ArtAuction.Core.Application.DTO;
+using MediatR;
+
+namespace ArtAuction.Core.Application.Commands
+{
+    public class GetAuctionMessagesCommand : IRequest<IEnumerable<MessageDto>>
+    {
+        public int AuctionNumber { get; set; }
+    }
+}
---
+++ src/ArtAuction.Core.Application/Handlers/GetAuctionMessagesCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ArtAuction.Core.Application.Commands;
+using ArtAuction.Core.Application.DTO;
+using ArtAuction.Core.Application.Interfaces.Repositories;
+using ArtAuction.Core.Domain.Enums;
+using MediatR;
+
+namespace ArtAuction.Core.Application.Handlers
+{
+    public class GetAuctionMessagesCommandHandler : IRequestHandler<GetAuctionMessagesCommand, IEnumerable<MessageDto>>
+    {
+        private readonly IAuctionRepository _auctionRepository;
+        private readonly IUserRepository _userRepository;
+
+        public GetAuctionMessagesCommandHandler(IAuctionRepository auctionRepository, IUserRepository userRepository)
+        {
+            _auctionRepository = auctionRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task<IEnumerable<MessageDto>> Handle(GetAuctionMessagesCommand request, CancellationToken cancellationToken)
+        {
+            var auction = await _auctionRepository.GetAuctionAsync(request.AuctionNumber);
+            var messages = new List<MessageDto>();
+            
+            foreach (var message in auction.Messages)
+            {
+                var user = _userRepository.GetUser(message.UserId);
+
+                messages.Add(new MessageDto
+                {
+                    UserLogin = user.Login,
+                    IsAdmin = user.Role == UserRole.Administrator,
+                    DateTime = message.DateTime,
+                    MessageText = message.MessageText
+                });
+            }
+
+            return messages;
+        }
+    }
+}
