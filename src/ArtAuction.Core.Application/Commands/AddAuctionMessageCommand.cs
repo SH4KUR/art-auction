174d5a90836ea63d1d2a0dd40767d54fb43bd703
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-06-16 16:11:40 +0300

    [ART-26] Added AddAuctionMessageCommandHandler

---
+++ src/ArtAuction.Core.Application/Commands/AddAuctionMessageCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace ArtAuction.Core.Application.Commands
+{
+    public class AddAuctionMessageCommand : IRequest
+    {
+        public int AuctionNumber { get; set; }
+        public string Login { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ src/ArtAuction.Core.Application/Handlers/AddAuctionMessageCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ArtAuction.Core.Application.Commands;
+using ArtAuction.Core.Application.Interfaces.Repositories;
+using ArtAuction.Core.Domain.Entities;
+using ArtAuction.Core.Domain.Enums;
+using MediatR;
+
+namespace ArtAuction.Core.Application.Handlers
+{
+    public class AddAuctionMessageCommandHandler : IRequestHandler<AddAuctionMessageCommand, Unit>
+    {
+        private readonly IAuctionRepository _auctionRepository;
+        private readonly IUserRepository _userRepository;
+
+        public AddAuctionMessageCommandHandler(IAuctionRepository auctionRepository, IUserRepository userRepository)
+        {
+            _auctionRepository = auctionRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task<Unit> Handle(AddAuctionMessageCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserAsync(request.Login);
+            var auction = await _auctionRepository.GetAuctionAsync(request.AuctionNumber);
+
+            var message = new Message
+            {
+                MessageId = Guid.NewGuid(),
+                AuctionId = auction.AuctionId,
+                UserId = user.UserId,
+                DateTime = DateTime.Now,
+                IsAdmin = user.Role == UserRole.Administrator,
+                MessageText = request.Message
+            };
+
+            await _auctionRepository.AddMessageAsync(message);
+            
+            return Unit.Value;
+        }
+    }
+}
