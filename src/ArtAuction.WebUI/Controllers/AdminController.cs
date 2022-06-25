cc2ac47fc556170f996af577e2b3569d5bb21a8d
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-06-26 02:04:15 +0300

    [ART-60, ART-51] Added Admin page with Complaints

---
+++ src/ArtAuction.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ArtAuction.Core.Application.Commands;
+using ArtAuction.WebUI.Models.Profile;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtAuction.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public AdminController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var complaints = await _mediator.Send(new GetComplaintsCommand());
+            return View(complaints.Select(c => _mapper.Map<ComplaintViewModel>(c)));
+        }
+    }
+}
